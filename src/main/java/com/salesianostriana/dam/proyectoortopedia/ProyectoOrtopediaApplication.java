0918d1825ed7015e05b1f015607b80e53ebe4528
Daniel Martinez <daniel1martinezleon@gmail.com>
2023-05-11 22:21:30 +0200

    primer commit

---
+++ src/main/java/com/salesianostriana/dam/proyectoortopedia/ProyectoOrtopediaApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.proyectoortopedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoOrtopediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoOrtopediaApplication.class, args);
+	}
+
+}
